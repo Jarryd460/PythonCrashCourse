1ac4b72008672ef3be16c83c4ff949f276d2b321
Jarryd Deane <Jarryd.deane@gmail.com>
2023-09-28 11:59:21 +0200

    Working with numbers and comments

---
+++ python_work/favourite_number.py
@@ -0,0 +1,4 @@
+# Assigns my favourite number to a variable and prints it out to the console
+favourite_number = 28
+message = f"My favourite number is {favourite_number}"
+print(message)
---
+++ python_work/mathematics.py
@@ -0,0 +1,6 @@
+# Maths sums that all result in 8
+print(5+3)
+print(10-2)
+print(2 * 4)
+print(16 / 2)
+print(2 ** 3)
